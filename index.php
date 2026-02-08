1bd5c4a0a6a4776e6012fea14ae5d835f9109830
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2026-02-09 00:02:14 +0100

    add array definition in genre and set 1 or more number of genres for movie

--- index.php
+++ index.php
@@ -19,16 +19,16 @@ class Movie
     public int $duration;
     public int $year;
     public $trama;
-    public $genre;
+    public array $genres;
 
-    function __construct($_title, $_director, $_duration, $_year, $_trama, Genre $_genre)
+    function __construct($_title, $_director, $_duration, $_year, $_trama, array $_genres)
     {
         $this->title = $_title;
         $this->director = $_director;
         $this->duration = $_duration;
         $this->year = $_year;
         $this->trama = $_trama;
-        $this->genre = $_genre;
+        $this->genres = $_genres;
     }
 
     public function getDurationinHours()
@@ -39,9 +39,13 @@ class Movie
     }
 }
 
-$titanic = new Movie("Titanic", "James Cameron", 194, 1997, "Sul transatlantico più famoso della storia, l'amore proibito tra Jack e Rose viene spezzato dal tragico scontro con un iceberg.",new Genre ("Tragedia","Bello"));
-$avatar = new Movie("Avatar", "James Cameron", 162, 2009, "Su un mondo alieno, un ex marine paralizzato intraprende una missione che lo porterà a lottare per proteggere la civiltà dei Na'vi.",new Genre ("Fantascienza","Bellissimo"));
-$matrix = new Movie("Matrix", "Lana & Lilly Wachowski", 136, 1999, "Un programmatore scopre che la realtà è una simulazione creata dalle macchine e si unisce a una ribellione per liberare l'umanità.",new Genre ("Fantascienza","Stupendo"));
+$drammatico = new Genre("Drammatico", "Storie profonde e coinvolgenti.");
+$romantico = new Genre("Romantico", "Focalizzato sulle relazioni amorose.");
+$fantascienza = new Genre("Fantascienza", "Esplorazione di concetti futuristici.");
+
+$titanic = new Movie("Titanic", "James Cameron", 194, 1997, "Sul transatlantico più famoso della storia, l'amore proibito tra Jack e Rose viene spezzato dal tragico scontro con un iceberg.",[$drammatico, $romantico]);
+$avatar = new Movie("Avatar", "James Cameron", 162, 2009, "Su un mondo alieno, un ex marine paralizzato intraprende una missione che lo porterà a lottare per proteggere la civiltà dei Na'vi.",[$fantascienza, new Genre("Azione", "Scene dinamiche e combattimenti")]);
+$matrix = new Movie("Matrix", "Lana & Lilly Wachowski", 136, 1999, "Un programmatore scopre che la realtà è una simulazione creata dalle macchine e si unisce a una ribellione per liberare l'umanità.",[$fantascienza] );
 
 var_dump($titanic);
 var_dump($avatar);
